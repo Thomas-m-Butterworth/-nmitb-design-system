f1badad83bf6e25ab2c1d2c6936506cf7c25a5e5
Thomas Butterworth <thomas.m.butterworth@gmail.com>
2025-01-07 15:31:14 +0000

    fonts enabled in dist

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Preview } from "@storybook/react";
 import { ThemeProvider, theme } from "../src/theme";
-import "../src/styles/fonts.css";
+import "../src/assets/fonts/fonts.css";
 
 const preview: Preview = {
   parameters: {
